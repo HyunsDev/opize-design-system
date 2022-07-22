0280502d464daad3dbe974f0204cc170478465a1
혀느현스 <hyuns@hyuns.dev>
2022-07-23 00:47:23 +0900

    add button & spinner

---
+++ src/lib/index.ts
@@ -0,0 +1,2 @@
+export * from './button/button'
+export * from './spinner/spinner'
---
+++ src/lib/spinner/spinner.tsx
@@ -0,0 +1,80 @@
+import styled, { keyframes } from "styled-components";
+import React from 'react';
+import '../style/var.css'
+
+interface Loader {
+    size: number;
+}
+
+const Loader = styled.div<Loader>`
+    position: relative;
+    margin: 0 auto;
+    width: ${props => props.size}px;
+    &:before {
+        content: '';
+        display: block;
+        padding-top: 100%;
+    }
+`
+
+const dash = keyframes`
+    0% {
+        stroke-dasharray: 1, 200;
+        stroke-dashoffset: 0;
+    }
+    50% {
+        stroke-dasharray: 89, 200;
+        stroke-dashoffset: -35px;
+    }
+    100% {
+        stroke-dasharray: 89, 200;
+        stroke-dashoffset: -124px;
+    }
+`
+
+const rotate = keyframes`
+    100% {
+        transform: rotate(360deg);
+    }
+`
+
+const Circular = styled.svg`
+    animation: ${rotate} 2s linear infinite;
+    height: 100%;
+    transform-origin: center center;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+
+    .path {
+        stroke-dasharray: 1, 200;
+        stroke-dashoffset: 0;
+        animation: ${dash} 1.5s ease-in-out infinite;
+        stroke: ${props => props.color || "#ffffff"};
+        stroke-linecap: round;
+    }
+`
+
+interface SpinnerProps {
+    size?: number;
+    color?: string;
+    strokeWidth?: number;
+}
+
+export const Spinner = ({
+    strokeWidth = 4,
+    color = 'var(--gray9)',
+    size = 32
+}: SpinnerProps) => {
+    return (
+        <Loader size={size}>
+            <Circular viewBox="25 25 50 50" color={color}>
+                <circle className="path" cx="50" cy="50" r="20" fill="none" strokeWidth={strokeWidth} strokeMiterlimit="10" />
+            </Circular>
+        </Loader>
+    )
+}
