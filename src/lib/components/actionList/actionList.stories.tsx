96c0a03d8a48379e58a234a99297e206f43d0650
혀느현스 <hyuns@hyuns.dev>
2022-08-06 02:58:41 +0900

    add actionList

---
+++ src/lib/components/actionList/actionList.stories.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { ActionList } from './';
+import styled from 'styled-components';
+
+
+export default {
+  title: 'Library/ActionList',
+  component: ActionList,
+  argTypes: {
+    isSticky: {
+        type: 'boolean'
+    }
+  },
+} as ComponentMeta<typeof ActionList>;
+
+const Template: ComponentStory<typeof ActionList> = (args) => (
+    <ActionList {...args}>
+        <ActionList.Item to={'/'}>Item</ActionList.Item>
+        <ActionList.Item to={'/'}>Item</ActionList.Item>
+        <ActionList.Divider />
+        <ActionList.Item to={'/'}>Item</ActionList.Item>
+    </ActionList>
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+
+};
+
+const StickyDiv = styled.div`
+    position: relative;
+    max-width: 300px;
+    padding-top: 128px;
+    margin: 0 auto;
+    height: 3000px;
+`
+
+const StickyTemplate: ComponentStory<typeof ActionList> = (args) => (
+    <StickyDiv>
+        <ActionList {...args}>
+            <ActionList.Item to={'/'}>Item</ActionList.Item>
+            <ActionList.Item to={'/'}>Item</ActionList.Item>
+            <ActionList.Divider />
+            <ActionList.Item to={'/'}>Item</ActionList.Item>
+        </ActionList>
+    </StickyDiv>
+);
+
+export const Sticky = StickyTemplate.bind({});
+Sticky.args = {
+    isSticky: true
+};
---
+++ src/lib/components/actionList/index.tsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+import styled, { css } from "styled-components";
+import { cv } from "../../style";
+
+
+const ActionListDiv = styled.div<{isSticky: Boolean, stickyTop?: number}>`
+    display: flex;
+    flex-direction: column;
+    ${ props => props.isSticky && css`
+        position: sticky;
+        top: ${props.stickyTop || 64}px;
+    `}
+`
+
+const ActionListItem = styled(Link)`
+    display: flex;
+    align-items: center;
+    padding: 8px 12px;
+    border-radius: 4px;
+    background-color: ${cv.bg_element7};
+    transition: background-color 100ms;
+    user-select: none;
+    font-size: 0.875rem;
+    text-decoration: none;
+    color: ${cv.text1};
+    line-height: 24px;
+    cursor: pointer;
+    &:hover {
+        background-color: ${props => props.color === 'red' ? cv.bg_red1 : cv.bg_element8};
+    }
+`
+
+const ActionListDivider = styled.div`
+    border-bottom: solid 1px ${cv.border3};
+    margin-top: 8px;
+    margin-bottom: 8px;
+`
+
+export const ActionList = Object.assign(ActionListDiv, {
+    Item: ActionListItem,
+    Divider: ActionListDivider
+})
