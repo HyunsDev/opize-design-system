62ea604704a2f816e92789bad998129d5cd78fec
혀느현스 <hyuns@hyuns.dev>
2022-08-06 01:59:30 +0900

    add ::selection style

--- src/lib/components/wrapper/index.tsx
+++ src/lib/components/wrapper/index.tsx
@@ -9,6 +9,9 @@ const GlobalStyles = createGlobalStyle`
         margin: 0;
         box-sizing: border-box;
         font-family: "Inter", -apple-system,BlinkMacSystemFont,"Segoe UI","Roboto","Oxygen","Ubuntu","Cantarell","Fira Sans","Droid Sans","Helvetica Neue",sans-serif;
+        ::selection {
+            background: #74fde0;
+        }
     }
 
     :root {
