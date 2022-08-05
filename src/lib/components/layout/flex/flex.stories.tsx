9a9a6da064dc84cd476731870b333c86c492bcd5
혀느현스 <hyuns@hyuns.dev>
2022-08-06 04:33:35 +0900

    remove flex layout with <Flex>

--- src/lib/components/layout/flex/flex.stories.tsx
+++ src/lib/components/layout/flex/flex.stories.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { Check } from 'phosphor-react'
 
-import { FlexBetween as FlexBetweenComp, FlexCenter as FlexCenterComp, FlexColumn as FlexColumnComp, FlexRow as FlexRowComp } from '.';
+import { Flex } from '.';
 import styled from 'styled-components';
 
 
 export default {
-  title: 'Library/FlexLayout',
-  component: FlexRowComp,
+  title: 'Library/Flex',
+  component: Flex,
   argTypes: {
     // backgroundColor: { control: 'color' },
   },
-} as ComponentMeta<typeof FlexRowComp>;
+} as ComponentMeta<typeof Flex>;
 
 
 const Item = styled.div`
@@ -22,26 +21,8 @@ const Item = styled.div`
   border-radius: 4px;
 `
 
-const Template: ComponentStory<typeof FlexRowComp> = (args) => <FlexRowComp {...args}><Item /><Item /><Item /></FlexRowComp>;
+const Template: ComponentStory<typeof Flex> = (args) => <Flex {...args}><Item /><Item /><Item /></Flex>;
 export const FlexRow = Template.bind({});
 FlexRow.args = {
-
-};
-
-const FlexColumnTemplate: ComponentStory<typeof FlexColumnComp> = (args) => <FlexColumnComp {...args}><Item /><Item /><Item /></FlexColumnComp>;
-export const FlexColumn = FlexColumnTemplate.bind({});
-FlexColumn.args = {
-
-};
-
-const FlexCenterCompTemplate: ComponentStory<typeof FlexCenterComp> = (args) => <FlexCenterComp {...args}><Item /><Item /><Item /></FlexCenterComp>;
-export const FlexCenter = FlexCenterCompTemplate.bind({});
-FlexCenter.args = {
-
-};
-
-const FlexBetweenCompTemplate: ComponentStory<typeof FlexBetweenComp> = (args) => <FlexBetweenComp {...args}><Item /><Item /><Item /></FlexBetweenComp>;
-export const FlexBetween = FlexBetweenCompTemplate.bind({});
-FlexBetween.args = {
 
 };
--- src/lib/components/layout/flex/index.tsx
+++ src/lib/components/layout/flex/index.tsx
@@ -1,34 +1,6 @@
 import styled from "styled-components";
 
-export const FlexRow = styled.div<{gap?: number}>`
-    display: flex;
-    align-items: center;
-    gap: ${props => props.gap || 8}px;
-`
-
-export const FlexColumn = styled.div<{gap?: number}>`
-    display: flex;
-    flex-direction: column;
-    gap: ${props => props.gap || 8}px;
-`
-
-export const FlexCenter = styled.div<{gap?: number}>`
-    display: flex;
-    gap: ${props => props.gap || 8}px;
-    align-items: center;
-    justify-content: center;
-`
-
-export const FlexBetween = styled.div<{gap?: number}>`
-    display: flex;
-    gap: ${props => props.gap || 8}px;
-    align-items: center;
-    justify-content: space-between;
-`
-
-export const Flex = styled.div<{
-    flexDirection?: 'row' | 'column',
-}>`
+export const Flex = styled.div`
     display: flex;
     gap: 8px;
 `
