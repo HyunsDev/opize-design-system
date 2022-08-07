7919b42298cd9dea311e09d62f61c4d542e7f56c
혀느현스 <hyuns@hyuns.dev>
2022-08-07 23:46:20 +0900

    add avatar

---
+++ src/lib/components/avator/index.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components"
+import { cv } from "../../style"
+
+const StyledAvatar = styled.img<{size: number}>`
+    width: ${props => props.size}px;
+    height: ${props => props.size}px;
+    border-radius: ${props => props.size}px;
+    border: solid 1px ${cv.border3};
+`
+
+export function Avatar({
+    src,
+    alt = '',
+    size = 24
+}: {
+    src: string,
+    alt?: string,
+    size?: number
+}) {
+    return (
+        <StyledAvatar size={size} src={src} alt={alt} />
+    )
+}
