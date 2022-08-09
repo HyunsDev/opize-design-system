b520e5a01e1d5462df70fecfabc91a0dac8efdeb
혀느현스 <hyuns@hyuns.dev>
2022-08-09 17:24:55 +0900

    update FlexLayout

--- src/lib/components/layout/flex/index.tsx
+++ src/lib/components/layout/flex/index.tsx
@@ -1,6 +1,30 @@
 import styled from "styled-components";
 import React from "react";
 
-export const Flex = styled.div`
+const FlexLayout = styled.div`
     display: flex;
 `
+
+const FlexCenter = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const FlexBetween = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+
+const FlexColumn = styled.div<{gap: string}>`
+    display: flex;
+    flex-direction: column;
+    ${props => props.gap || props.gap};
+`
+
+export const Flex = Object.assign(FlexLayout, {
+    Center: FlexCenter,
+    Between: FlexBetween,
+    Column: FlexColumn
+})
