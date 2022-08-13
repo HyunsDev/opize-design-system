527ddfedbb49778c2aba430d164ad7058bd7f1ff
혀느현스 <hyuns@hyuns.dev>
2022-08-13 23:56:53 +0900

    update cv, add sv

--- src/lib/style/cssVariable.tsx
+++ src/lib/style/cssVariable.tsx
@@ -1,5 +1,23 @@
 interface CssVariable {
     vh: string
+
+    fontFamily: string
+    fontFamilyMonospace: string;
+
+    fontWeightRegular: number;
+    fontWeightSemiBold: number;
+
+    fontSizeNormal: string;
+    fontSize200: string;
+    fontSize300: string;
+    fontSize400: string;
+    fontSize500: string;
+    fontSize600: string;
+
+    lineHeightNormal: string;
+    lineHeight200: string;
+    lineHeight300: string;
+    lineHeight400: string;
 }
 
 type VariableKey = keyof CssVariable
@@ -7,6 +25,23 @@ type CssPalette = Record<VariableKey, string>;
 
 const cssVariable: CssVariable = {
     vh: '1vh',
+    fontFamily: `"Inter", 'Noto Sans KR', -apple-system,BlinkMacSystemFont,"Segoe UI","Roboto","Oxygen","Ubuntu","Cantarell","Fira Sans","Droid Sans","Helvetica Neue",sans-serif`,
+    fontFamilyMonospace: `Consolas, 'Courier New', Courier, monospaceFont family fontFamilyMonospace`,
+
+    fontWeightRegular: 400,
+    fontWeightSemiBold: 600,
+
+    fontSizeNormal: 'var(--fontSize400)',
+    fontSize200: '10px',
+    fontSize300: '12px',
+    fontSize400: '14px',
+    fontSize500: '16px',
+    fontSize600: '20px',
+
+    lineHeightNormal: 'var(--lineHeight400)',
+    lineHeight200: '16px',
+    lineHeight300: '20px',
+    lineHeight400: '24px',
 };
 
 const buildCssVariable = (variables: CssVariable) => {
---
+++ src/lib/style/cv.tsx
@@ -0,0 +1,14 @@
+import { builtCssVariable, cssPalette } from './cssVariable'
+import { themes, themedPalette } from './themeVariables'
+
+const cv = {
+    ...themedPalette,
+    ...cssPalette
+}
+
+const cssVar = (name: string) => `var(--${name.replace(/_/g, '-')})`;
+
+export {
+    cv, themes, builtCssVariable, cssVar
+}
+
